f539d26f3e5bd81974e468250801d0047cc1452a
dadi <1442374919@qq.com>
2020-02-09 18:35:04 +0800

    epidemic

---
+++ epidemic/epidemic/spiders/epidemicspider.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class EpidemicspiderSpider(scrapy.Spider):
+    name = 'epidemicspider'
+    allowed_domains = ['voice.baidu.com']
+    start_urls = ['https://voice.baidu.com/act/newpneumonia/newpneumonia/']
+
+    def parse(self, response):
+        items = []
+        sub_selector = response.xpath('//*[@id="ptab-0"]/div[2]/table/tbody/tr/td[1]/div/div[1]').extract()
